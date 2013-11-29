740bd02928ea4c143deb7a7486a0639f471704d3
Cody Jackson <sxenog@gmail.com>
2013-11-29 00:44:41 -0800

    Got rid of the unnecessary crap.

--- utility_pwm_reading.ino
+++ utility_pwm_reading.ino
@@ -1,21 +1,16 @@
 #include "pwm_parallel_reader.h"
-#include "pwm_parallel_writer.h"
-
 #include <Arduino.h>
 
 
 Pwm::InputPin receiver(5);
-
 void setup()
 {
 	Serial.begin(9600);
 	receiver.initialize();
-	Time::Milliseconds ms(1000);
 
 }
 
 unsigned long cur = 0;
-
 void loop()
 {
 	receiver.tick();
